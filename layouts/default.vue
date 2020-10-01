5b76b2999fea2d86f13dc22cc673332397ea7b26
François KUSZTOS <francois.kusztos@smile.fr>
2020-10-01 11:52:53 +0200

    Add component to layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
+  <div class="container">
+    <TheNavbar />
     <nuxt />
   </div>
 </template>
+
+<script>
+import TheNavbar from '~/components/TheNavbar.vue'
+export default {
+  components: {
+    TheNavbar
+  }
+}
+</script>
